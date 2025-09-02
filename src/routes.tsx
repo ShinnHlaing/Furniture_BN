e4bf63ba34faa6a49ca0126bbffebdd03e66b09f
ShinnHlaing <hlaingthazinphoo.hp@gmail.com>
2025-09-02 22:42:00 +0630

    add blog-detail

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,8 @@ import RootLayout from "@/pages/RootLayout";
 import Home from "@/pages/Home";
 import Erro from "@/pages/Erro";
 import About from "@/pages/About";
+import Blog from "@/pages/blogs/Blog";
+import BlogDetail from "@/pages/blogs/BlogDetail";
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -11,6 +13,8 @@ export const router = createBrowserRouter([
     children: [
       { index: true, Component: Home },
       { path: "about", Component: About }, //relative path
+      { path: "blogs", Component: Blog }, //relative path
+      { path: "blogs/:postId", Component: BlogDetail }, //relative path
     ],
   },
 ]);
