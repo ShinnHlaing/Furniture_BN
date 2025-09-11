af4a069ab9c299c2760452605c842e6f6e93eedf
ShinnHlaing <hlaingthazinphoo.hp@gmail.com>
2025-09-11 22:41:49 +0630

    add: Pagination & PageFiler

--- src/pages/products/Product.tsx
+++ src/pages/products/Product.tsx
@@ -1,5 +1,33 @@
+import ProductCard from "@/components/products/ProductCard";
+import { products, filterList } from "@/data/products";
+import { Pagination } from "@/components/ui/pagination";
+import ProductFilter from "@/components/products/ProductFilter";
 function Product() {
-  return <div>Product</div>;
+  return (
+    <div className="">
+      <section className="">
+        <section className="">
+          <ProductFilter
+            title="Furnitures Made By"
+            filterList={filterList.categories}
+          />
+          <ProductFilter
+            title="Furnitures Types"
+            filterList={filterList.types}
+          />
+        </section>
+        <section className="">
+          <h1 className="">All Products</h1>
+          <div className="">
+            {products.map((product) => (
+              <ProductCard products={product} key={product.id} />
+            ))}
+          </div>
+          <Pagination />
+        </section>
+      </section>
+    </div>
+  );
 }
 
 export default Product;
